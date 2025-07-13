dacf02d6e013c535e81d05b94367550dcc272b23
JMS1320 <87022786+JMS1320@users.noreply.github.com>
2025-07-13 15:43:49 -0300

    Create page.tsx

---
+++ app/importador-nuevo/page.tsx
@@ -0,0 +1,9 @@
+import ImportadorExcelDinamico from "@/components/importador-excel-dinamico"
+
+export default function Page() {
+  return (
+    <main className="p-8">
+      <ImportadorExcelDinamico />
+    </main>
+  )
+}
